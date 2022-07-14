0334803088c921104c0e9ed4fcf4fe52316f961d
Casios1983 <62154818+Casios1983@users.noreply.github.com>
2022-07-14 12:20:41 -0300

    Merge

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp4.Models;
+
+var db = new NorthwindContext();
+
+var listado = db.Products.OrderByDescending(x => x.UnitsInStock)
+                       .Where(x => x.SupplierId == 12).ToList();
+calular(126, listado);
+
+void calular(short? embargo, List<Product> listar)
+{
+ 
+        short? total = 0;
+
+    foreach (var item in listar)
+    {
+      total = (short?)(item.UnitsInStock - embargo); 
+    }
+    
+}
